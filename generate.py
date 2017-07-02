d5ba414c14ec6f04c3c9fecef409b7e3dc38b4d0
Nathan Woodward <nathanrichardwoodward@gmail.com>
2017-07-02 18:00:15 +0100

    ddd

---
+++ generate.py
@@ -0,0 +1,18 @@
+#!/bin/env python
+
+COSTS = [
+    ("Tactical Marine", 13),
+    ("Flamer", 9),
+    ("Missile Launcher", 25),
+    ("Rhino", 70),
+    ("Storm Bolter", 2)
+    ("Death Company (Jump Pack)", 20)
+]
+
+AL = (
+    "Tactical Squad",
+    [
+        ("Tactical Marine", 10),
+        ("Flamer", 1)
+    ]
+)
